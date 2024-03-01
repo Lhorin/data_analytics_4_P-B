212281561705861931d16edceaeffa617627dc85
AndyCao-GitHub <andyfunc@hotmail.com>
2024-03-01 16:01:45 +0100

    Rmd files

--- First viz_electricity.R
+++ First viz_electricity.R
@@ -2,8 +2,6 @@ dat <- read.csv("C:\\Users\\andyf\\OneDrive\\Desktop\\Fürs Studium\\9 Data Anal
 
 nrow(dat)/365
 
-library(tidyverse)
-
 dat2 <- dat %>% group_by(Jahr,Monat,Tag) %>% summarize(stromverbrauch_sum = sum(Stromverbrauch))
 
 dat2 <- dat2 %>% mutate(Time = str_c(Monat, Tag, sep = "-"))
@@ -11,8 +9,6 @@ dat2 <- dat2 %>% mutate(Time = str_c(Monat, Tag, sep = "-"))
 dat2 %>% ggplot(aes(Time, stromverbrauch_sum, group = Jahr, col = factor(Jahr)))+
    geom_line()
 
-dat2 %>% ggplot(aes(factor(Monat), stromverbrauch_sum, group = Jahr, col=factor(Jahr)))+
-   geom_line()
 
 
 dat2 %>% filter(Jahr == 2021 | Jahr== 2023) %>% ggplot(aes(Time, stromverbrauch_sum, group = Jahr, col = factor(Jahr)))+
